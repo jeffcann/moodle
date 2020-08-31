b0c96d26fe7c28272b39a353c471c7e9af3ce33a
Jeff Cann <jeff@constant.consulting>
2020-08-31 13:21:03 +1000

    MATT-11: Auto scroll on smaller tablets

--- question/type/musicaldictation/renderer.php
+++ question/type/musicaldictation/renderer.php
@@ -42,7 +42,7 @@ class qtype_musicaldictation_renderer extends qtype_renderer {
         $currentanswer = $qa->get_last_qt_var('answer');
 
         $this->page->requires->js('/question/type/musicaldictation/amd/setup.js');
-        $this->page->requires->js('/question/type/musicaldictation/amd/app.04035336.js');
+        $this->page->requires->js('/question/type/musicaldictation/amd/app.3995aa01.js');
 
         $inputname = $qa->get_qt_field_name('answer');
         $inputattributes = array(
