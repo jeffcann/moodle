411b2f9b14a55d0d230c9c0368da1d1f9811e80a
Jeff Cann <jeff@constant.consulting>
2020-09-18 16:14:24 +1000

    Update dictation version

--- question/type/musicaldictation/renderer.php
+++ question/type/musicaldictation/renderer.php
@@ -42,7 +42,7 @@ class qtype_musicaldictation_renderer extends qtype_renderer {
         $currentanswer = $qa->get_last_qt_var('answer');
 
         $this->page->requires->js('/question/type/musicaldictation/amd/setup.js');
-        $this->page->requires->js('/question/type/musicaldictation/amd/app.9fecb1d9.js');
+        $this->page->requires->js('/question/type/musicaldictation/amd/app.5e4c56a0.js');
 
         $inputname = $qa->get_qt_field_name('answer');
         $inputattributes = array(
